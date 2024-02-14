a6081584af87d3278f12c165ae2301a880664194
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-02-14 15:42:40 -0300

    refactor: remove icon

--- src/components/Icon/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Icon: React.FC = () => (
-	<img src={require("./IconIMG.png")} height={35} width={35} />
-);
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -8,13 +8,12 @@ import {
 import dotenv from "dotenv";
 import cloudinaryPlugin from "payload-cloudinary-plugin/dist/plugins";
 import { buildConfig } from "payload/config";
+import { CodeBlock } from "./blocks/CodeBlock";
 import Article from "./collections/Article";
 import Category from "./collections/Category";
 import Media from "./collections/Media";
 import AboutBlog from "./globals/AboutBlog";
 import Portfolio from "./globals/Portfolio";
-import { CodeBlock } from "./blocks/CodeBlock";
-import { Icon } from "../src/components/Icon";
 
 dotenv.config();
 
@@ -22,12 +21,6 @@ export default buildConfig({
 	serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
 	admin: {
 		bundler: webpackBundler(),
-		components: {
-			graphics: {
-				Logo: Icon,
-				Icon,
-			},
-		},
 	},
 	collections: [Article, Media, Category],
 	globals: [Portfolio, AboutBlog],
